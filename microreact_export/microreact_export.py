e0c6b02e3f2b213558c6dd7048c5a528103542b4
awh082834 <andrew.hale@theiagen.com>
2025-10-23 14:18:57 +0000

    Initial microreact_export

---
+++ microreact_export/microreact_export.py
@@ -0,0 +1,315 @@
+import argparse
+import pandas as pd
+import requests
+import json
+import base64
+import uuid
+from pathlib import Path
+from typing import Optional, List
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+def parse_metadata_tsv(tsv: Path, id_column: str, selected_columns: Optional[List[str]] = None):
+  with open(tsv, 'r', newline='', encoding='utf-8') as f:
+    df = pd.read_csv(f, sep="\t", dtype=str)
+    logger.debug(f"selected_columns: {selected_columns}")
+    logger.info(f"Original metadata TSV has {len(df)} records and columns: {df.columns.tolist()}")
+    if selected_columns:
+      data = df[selected_columns]
+    else:
+      logger.info("No selected columns provided; using all columns.")
+      data = df
+    logger.info(f"Parsed metadata TSV with {len(data)} records and columns: {data.columns.tolist()}")
+    logger.info(f"First few rows of data:\n{data.head()}")
+    csv_data = data.to_csv(index=False)
+    f.close()
+  
+  headers = csv_data.splitlines()[0].split(',')
+  if id_column not in headers:
+    logger.error(f"ID column '{id_column}' not found in metadata headers: {headers}")
+    logger.info(f"Defaulting to first column '{headers[0]}' as ID column.")
+    id_column = headers[0]
+
+  return csv_data, headers, id_column
+
+def encode(data: str) -> str:
+  encoded_data = "data:application/octet-stream;base64," + base64.b64encode(data.encode('utf-8')).decode('utf-8')
+  file_id = str(uuid.uuid4())
+  logger.info(f"File encoded with ID: {file_id}")
+  return encoded_data, file_id
+
+def add_trees(tree_files: List[Path], id_column: str):
+  tree_files_dict = {}
+  tree_dict = {}
+  for tree_file in tree_files:
+    logger.info(f"Processing tree file: {tree_file}")
+    with open(tree_file, 'r', encoding='utf-8') as tf:
+      tree_content = tf.read()
+      tree_encoded, tree_id = encode(tree_content)
+      logger.info(f"Tree file encoded with ID: {tree_id}")
+      tree_files_dict[tree_id] = {
+          "blob": tree_encoded,
+          "format": "text/x-nh",
+          "file": tree_id,
+          "name": tree_file.name,
+          "type": "tree"
+      }
+      tree_dict[f"tree_{tree_files.index(tree_file)+1}"] = {
+          "type": "rc",
+          "labelField": id_column,
+          "title": f"Tree {tree_files.index(tree_file)+1}",
+          "showLabels": True,
+          "showLeafLabels": True,
+          "file": tree_id
+      }
+    return tree_files_dict, tree_dict
+
+def create_project_entry(
+  entry_type: str, 
+  metadata: Optional[Path], 
+  id_column: str, 
+  selected_columns: Optional[List[str]],
+  tree_files: Optional[List[Path]]
+):
+  if entry_type == "metadata":
+    logger.info("Creating metadata entry")
+    metadata_parsed, headers, id_column = parse_metadata_tsv(metadata, id_column, selected_columns)
+    metadata_encoded, entry_id = encode(metadata_parsed)
+    metadata_entry = {}
+    metadata_entry[entry_id] = {
+        "blob": metadata_encoded,
+        "format": "text/csv",
+        "id": entry_id,
+        "name": "metadata.csv",
+        "type": "data"
+    }
+    return metadata_entry, entry_id, headers
+  elif entry_type == "tree":
+    logger.info("Creating tree entries")
+    tree_files_dict, tree_dict = add_trees(tree_files, id_column)
+    return tree_files_dict, tree_dict
+  elif entry_type == "map":
+    logger.info("Creating map entry")
+    map_entry = {}
+    map_entry_id = str(uuid.uuid4())
+    map_entry[map_entry_id] = {
+        "dataType": "geographic-coordinates",
+        "title": "Map",
+        "latitudeField": "latitude",
+        "longitudeField": "longitude",
+        "type": "mapbox",
+        "coordinate-unit": "decimal-degrees",
+        "showRegions": True,
+        "showRegionOutlines": True,
+        "regionsColourMethod": "entries",
+        "regionsColourOpacity": 100,
+        "regionsColourPalette": "ColorBrewer YlOrBr-2"
+    }
+    return map_entry
+
+def create_microreact_project(
+    metadata: Path,
+    id_column: str,
+    tree_files: Optional[List[Path]],
+    access_token: Optional[str],
+    restricted_access: bool = True,
+    project_name: str = "New Microreact Project",
+    selected_columns: Optional[List[str]] = None
+):
+
+  metadata_entry, metadata_id, headers = create_project_entry("metadata", metadata, id_column, selected_columns, None)
+
+  # Create Metadata Dataset and Table Entries
+  dataset_id = str(uuid.uuid4())
+  logger.info(f"Created dataset entry with ID {dataset_id}, linked to metadata file ID {metadata_id}")
+  dataset_entry = {
+      "id": dataset_id,
+      "file": metadata_id,
+      "idFieldName": id_column
+  }
+  columns = [{"field": col} for col in selected_columns] if selected_columns else [{"field": col} for col in headers]
+  table_id = str(uuid.uuid4())
+  logger.info(f"Created table entry with ID {table_id}, linked to metadata file ID {metadata_id}")
+  table_entry = {
+    "title": "Metadata",
+    "file": metadata_id,
+    "columns": columns
+  }    
+
+  if tree_files:
+    tree_files_dict, tree_dict = create_project_entry("tree", None, id_column, None, tree_files)
+  else:
+    logger.info("No tree files provided; skipping tree entry creation.")
+    tree_files_dict, tree_dict = {}, {}
+  
+  if "latitude" in headers and "longitude" in headers:
+    map_entry = create_project_entry("map", None, "", None, None)
+    logger.info("Map entry created based on presence of latitude and longitude in metadata.")
+  else:
+    logger.info("No latitude/longitude fields found; skipping map entry creation.")
+    map_entry = {}
+
+  project_input = {
+    "schema": "https://microreact.org/schema/v1.json",
+    "meta": {
+      "name": project_name
+    },
+    "datasets": {
+      dataset_id: dataset_entry
+    },
+    "files": {
+      **metadata_entry,
+      **tree_files_dict
+    },
+    "tables": {
+      table_id: table_entry
+    },
+    "trees": {
+      **tree_dict
+    },
+    "charts": {},
+    "filters": {},
+    "maps": {
+      **map_entry
+    },
+    "networks": {},
+    "notes": {},
+    "panes": {},
+    "slicers": {},
+    "styles": {},
+    "timelines": {},
+    "views": {},
+    "matrices": {}
+  }
+
+  # Write Input to JSON
+  project_input_json = json.dumps(project_input)
+  logger.info(f"Created Microreact project JSON")
+
+  if access_token:
+    url = "https://microreact.org/api/projects/create"
+    if restricted_access:
+      url += "?access=private"
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Access-Token": access_token
+    }
+    response = requests.post(url, headers=headers, json=project_input)
+    logger.info(f"Microreact API response status: {response.status_code}")
+    if response.status_code == 200:
+      microreact_response = response.json()
+      logger.info(f"Microreact project created successfully with ID: {microreact_response.get('id')}")
+    else:
+      logger.error(f"Failed to create Microreact project: {response.text}")
+
+  return microreact_response, project_input_json
+
+def update_microreact_project(
+    project_url: str,
+    access_token: str,
+    metadata: Optional[str],
+    id_column: Optional[str],
+    tree_files: Optional[List[Path]]
+):
+  logger.info(f"Updating existing Microreact project at {project_url}")
+  url = f"https://microreact.org/api/projects/update?project={project_url}"
+  get_url = f"https://microreact.org/api/projects/json?project={project_url}"
+  get_request_headers = {
+    "Access-Token": access_token
+  }
+  get_response = requests.get(get_url, headers=get_request_headers)
+  updated_project = get_response.json()
+  logger.info(f"Fetched current project data for update")
+
+  if metadata:
+    metadata_entry, metadata_id, headers = create_project_entry("metadata", metadata, id_column, None, None)
+
+    if "latitude" in headers and "longitude" in headers:
+      print(updated_project.get("maps"))
+      if updated_project.get("maps") is None:
+        map_entry = create_project_entry("map", None, "", None, None)
+        updated_project["maps"] = {
+          **updated_project.get("maps", {}),
+          **map_entry
+        }
+        logger.info("Updated project with new map entry based on metadata")
+
+    new_dataset_id = str(uuid.uuid4())
+    logger.info(f"Created new dataset entry with ID {new_dataset_id}, linked to metadata file ID {metadata_id}")
+    updated_project["datasets"] = {
+      new_dataset_id: {
+        "id": new_dataset_id,
+        "file": metadata_id,
+        "idFieldName": id_column
+      }
+    }
+    columns = [{"field": col} for col in headers]
+    new_table_id = str(uuid.uuid4())
+    logger.info(f"Created new table entry with ID {new_table_id}, linked to metadata file ID {metadata_id}")
+    updated_project["tables"] = {
+      new_table_id: {
+        "title": "Metadata",
+        "file": metadata_id,
+        "columns": columns
+      }
+    }
+    updated_project["files"].update(metadata_entry)
+    logger.info("Updated project with new metadata")
+  
+  if tree_files:
+    tree_files_dict, tree_dict = create_project_entry("tree", None, id_column, None, tree_files)
+    updated_project["files"].update(tree_files_dict)
+    updated_project["trees"].update(tree_dict)
+    logger.info("Updated project with new tree files")
+
+  post_headers = {
+    "Content-Type": "application/json; charset=UTF-8",
+    "Access-Token": access_token
+  }
+  post_response = requests.post(url, headers=post_headers, json=updated_project)
+  return post_response, updated_project
+
+def main():
+  argparser = argparse.ArgumentParser(description="Create a Microreact project from metadata and tree files.")
+  argparser.add_argument("--project_name", type=str, help="Name of the Microreact project")
+  argparser.add_argument("--project_url", type=str, help="URL of the Microreact project, used for updating existing projects")
+  argparser.add_argument("--metadata_tsv", type=Path, help="Path to the metadata file")
+  argparser.add_argument("--tree_files", nargs="*", type=Path, help="Paths to the tree files")
+  argparser.add_argument("--selected_columns", nargs="*", type=str, help="Columns to include in the Microreact table")
+  argparser.add_argument("--access_token", type=str, help="Access token for Microreact API")
+  argparser.add_argument("--restricted_access", type=bool, default=True, help="Set project access to private if True")
+  argparser.add_argument("--update", type=bool, default=False, help="Update an existing Microreact project if True, pair with --project_url")
+  argparser.add_argument("--id_column", type=str, help="Column to use as the unique ID field")
+  args = argparser.parse_args()
+
+  if args.update and args.project_url:
+    microreact_response, updated_project = update_microreact_project(
+        project_url=args.project_url,
+        access_token=args.access_token,
+        metadata=args.metadata_tsv,
+        id_column=args.id_column,
+        tree_files=args.tree_files
+    )
+    with open("microreact_update_response.json", "w", encoding="utf-8") as response_file:
+      json.dump(microreact_response.json(), response_file, ensure_ascii=False, indent=2)
+    with open("updated_project_input.json", "w", encoding="utf-8") as project_file:
+      project_file.write(json.dumps(updated_project))
+  else:
+    microreact_response, project_json = create_microreact_project(
+      metadata=args.metadata_tsv,
+      tree_files=args.tree_files,
+      access_token=args.access_token,
+      restricted_access=args.restricted_access,
+      project_name=args.project_name,
+      id_column=args.id_column,
+      selected_columns=args.selected_columns,
+      project_url=args.project_url
+    )
+    with open("microreact_response.json", "w", encoding="utf-8") as response_file:
+      json.dump(microreact_response, response_file, ensure_ascii=False, indent=2)
+    with open("project_input.json", "w", encoding="utf-8") as project_file:
+      project_file.write(project_json)
+if __name__ == "__main__":
+  main()
