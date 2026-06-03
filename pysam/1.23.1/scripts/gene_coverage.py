61f234ac0d289648f60d8b803d5af83ddf4abb4f
xonq <konkelzach@tuta.io>
2026-06-03 20:48:52 +0000

    tes

--- pysam/1.23.1/scripts/gene_coverage.py
+++ pysam/1.23.1/scripts/gene_coverage.py
@@ -1,4 +1,5 @@
 import sys
+import json
 import pysam
 import logging
 import argparse
@@ -19,7 +20,7 @@ def exact_check(query_set: set, id: str) -> bool:
 
 def substring_check(query_set: set, id: str) -> bool:
     """Return True or False for a substring match"""
-    return any(id in query for query in query_set)
+    return any(query in id for query in query_set)
 
 
 def write_json(filename: str, data: dict) -> None:
@@ -37,7 +38,7 @@ def parse_gbff(
     query_set: set,
     feature_type: str,
     feature_qualifier: str,
-    id_check: function,
+    id_check: object,
     contig2query2coords: dict,
 ) -> dict:
     """Parse a GBFF to obtain query coordinates"""
@@ -45,15 +46,16 @@ def parse_gbff(
         for record in SeqIO.parse(handle, "genbank"):
             for feature in record.features:
                 # is this the feature we want to scan?
-                if feature.type.lower() == feature_type:
+                if feature.type.lower() == feature_type.lower():
                     # is there the qualifier that we want?
-                    qualifier_id = feature.qualifiers.get(feature_qualifier).strip()
-                    if qualifier_id:
+                    qualifier_ids = feature.qualifiers.get(feature_qualifier.strip())
+                    if qualifier_ids:
+                        qualifier_id = qualifier_ids[0]
                         # is this a qualifying feature?
                         if id_check(query_set, qualifier_id):
                             if qualifier_id in contig2query2coords[record.name]:
-                                print(
-                                    f"WARNING: {qualifier_id} recovered multiple times in {record.name}"
+                                logger.warning(
+                                    f"{qualifier_id} recovered multiple times in {record.name}"
                                 )
                             # GenBanks are 1-based coordinates, which we need to adjust for PySam
                             contig2query2coords[record.name][qualifier_id] = (
@@ -64,7 +66,7 @@ def parse_gbff(
 
 
 def parse_bed(
-    bedfile: str, query_set: set, id_check: function, contig2query2coords: dict
+    bedfile: str, query_set: set, id_check: object, contig2query2coords: dict
 ) -> dict:
     """Parse a BED file to obtain query coordinates"""
     with open(bedfile, "r") as handle:
@@ -83,12 +85,12 @@ def quantify_gene_coverage(
     bamfile: str, contig2query2coords: dict, min_depth: int = 1
 ) -> tuple:
     """Quantify gene breadth and depth off coverage"""
-    imported_bam = pysam.ALignmentFile(bamfile)
+    imported_bam = pysam.AlignmentFile(bamfile)
 
     depth_dict = {}
     coverage_dict = {}
 
-    for contig in query2coords in contig2query2coords.items():
+    for contig, query2coords in contig2query2coords.items():
         for query, coords in query2coords.items():
             if query in depth_dict:
                 logger.warning(
@@ -100,12 +102,10 @@ def quantify_gene_coverage(
             coverages = []
             for i, pos in enumerate(range(coords[0], coords[1])):
                 # calculate total depth across bases
-                total_depth = sum(
-                    coverage_data[0][i]
-                    + coverage_data[1][i]
-                    + coverage_data[2][i]
+                total_depth = coverage_data[0][i] \
+                    + coverage_data[1][i] \
+                    + coverage_data[2][i] \
                     + coverage_data[3][i]
-                )
                 # base is considered covered if beyond minimum depth
                 coverages.append(total_depth >= min_depth)
                 depths.append(total_depth)
@@ -116,19 +116,19 @@ def quantify_gene_coverage(
     return depth_dict, coverage_dict
 
 
-def make_csv(depth_dict: dict, coverage_dict: dict) -> str:
-    """Make a readable CSV to convey depth and coverage"""
-    csv_str = "#gene,average_depth,percent_coverage\n"
+def make_tsv(depth_dict: dict, coverage_dict: dict) -> str:
+    """Make a readable TSV to convey depth and coverage"""
+    tsv_str = "#gene\taverage_depth\tpercent_coverage\n"
     for query, depth in depth_dict.items():
-        csv_str += f"{query},{depth},{coverage_dict[query]}\n"
-    return csv_str.strip()
+        tsv_str += f"{query}\t{depth}\t{coverage_dict[query]}\n"
+    return tsv_str.strip()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="task_gene_coverage.wdl dependency script (github.com/theiagen/public_health_bioinformatics)"
     )
-    parser.add_argument("--bamfile", required=True)
+    parser.add_argument("--bam", required=True)
     parser.add_argument("--bedfile")
     parser.add_argument("--reference_gbff")
     parser.add_argument("--query_genes", required=True, nargs="+")
@@ -160,8 +160,7 @@ if __name__ == "__main__":
             args.feature_type,
             args.feature_qualifier,
             id_check,
-            contig2query2coords,
-            id_check,
+            contig2query2coords
         )
     if args.bedfile:
         contig2query2coords = parse_bed(
@@ -169,12 +168,12 @@ if __name__ == "__main__":
         )
 
     depth_dict, coverage_dict = quantify_gene_coverage(
-        args.bamfile, contig2query2coords, args.min_depth
+        args.bam, contig2query2coords, args.min_depth
     )
 
-    csv_str = make_csv(depth_dict, coverage_dict)
-    with open("COVERAGE_STATS.csv", "w") as out:
-        out.write(csv_str)
+    tsv_str = make_tsv(depth_dict, coverage_dict)
+    with open("COVERAGE_STATS.tsv", "w") as out:
+        out.write(tsv_str)
 
     write_json("DEPTH_DICT.json", depth_dict)
     write_json("COVERAGE_DICT.json", coverage_dict)
